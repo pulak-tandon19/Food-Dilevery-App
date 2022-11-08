8f785fc5e234d151ffacfb07c65deeab8e8ff5f7
Pulak Tandon <pulaktandon2000@gmail.com>
2022-11-08 12:30:57 +0530

    Environment Variables

--- deliver/settings.py
+++ deliver/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool, default = False)
+# DEBUG = True
 
 ALLOWED_HOSTS = [config('ALLOWED_HOST')]
 
